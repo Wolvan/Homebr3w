7256cea78647aeaadb1dc01cfb68f1bfa2534fce
Wolvan <wolvan1@gmail.com>
2016-07-12 20:06:44 +0200

    Fix User-Agent
    The first version of the PHP proxy actually didn't set the User-Agent
    properly due to me forgetting a `User-Agent: ` in front of `Homebr3w/`.
    This is fixed.
    
    Unfortunately, there is still an issue in getcia.php that requires
    attention. For some reason downloading a `.cia` with `file_get_contents`
    works fine, but trying to use the `http_req` curl function only returns
    `[]`.

--- PHP/include.php
+++ PHP/include.php
@@ -3,10 +3,8 @@
 	
 	function http_req($data = array("action" => "list", "fields" => array("id", "titleid", "author", "description", "name", "create_time", "update_time", "size", "mtime")), $endpoint = "v0/") {
 		$curl = curl_init($GLOBALS["api_path"].$endpoint);
-		$headers = array();
-		$headers[] = "Homebr3w/1.0.0 Homebr3w-Proxy/1.0.0";
 		curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($curl, CURLOPT_USERAGENT, "Homebr3w/1.0.0 Homebr3w-Proxy/1.0.1");
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		$response = curl_exec($curl);
 		curl_close($curl);

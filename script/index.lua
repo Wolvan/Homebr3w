e9ee9cadcbd1090353be7b9904155e79eeea1370
Wolvan <wolvan1@gmail.com>
2016-07-16 23:17:05 +0200

    Release Version 1.1.0

--- script/index.lua
+++ script/index.lua
@@ -24,7 +24,7 @@ local YELLOW = Color.new(255,205,66)
 local RED = Color.new(255,0,0)
 local GREEN = Color.new(55,255,0)
 
-local APP_VERSION = "1.0.0"
+local APP_VERSION = "1.1.0"
 local APP_DIR = "/Homebr3w"
 local APP_CACHE = APP_DIR.."/Cache"
 local APP_CONFIG = APP_DIR.."/config.json"
